239bc4bdd984b9afa9ffd3ae84b1ee640882f8c6
Parker Smith <parker@parkersmith.us>
2014-09-27 18:42:13 +0000

    Add test to movie collection spec

--- spec/MovieCollectionSpec.php
+++ spec/MovieCollectionSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec;
 
+use Movie;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,5 +11,10 @@ class MovieCollectionSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('MovieCollection');
+    }
+
+    function it_stores_a_collection_of_movies(Movie $movie)
+    {
+        $this->add($movie);
     }
 }
